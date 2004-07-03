4998987193d397547e21a5628802ea07962fea12
Aidan Lister <aidan@php.net>
2004-07-03 15:36:23 +0000

    Updated license
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_GameServerQuery/trunk@162598 c90b9560-bf6c-de11-be94-00142212c4b1

--- GameServerQuery/Protocols.php
+++ GameServerQuery/Protocols.php
@@ -1,4 +1,24 @@
 <?php
+// +----------------------------------------------------------------------+
+// | PHP version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2004 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3.0 of the PHP license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.php.net/license/3_0.txt.                                  |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors: Aidan Lister <aidan@php.net>                                |
+// |          Tom Buskens <ortega@php.net>                                |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
+
 $protocol['halflife']['name']				= 'Half-Life';
 $protocol['halflife']['packet']				= "\xFF\xFF\xFF\xFF%s\x00";
 $protocol['halflife']['queryport']			= '27015';
