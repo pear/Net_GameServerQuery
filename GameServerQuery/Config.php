11e8b8c843bd10894ef935e571b71e1d989ba7f0
Aidan Lister <aidan@php.net>
2004-07-05 17:07:17 +0000

    de-tabbed
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_GameServerQuery/trunk@162684 c90b9560-bf6c-de11-be94-00142212c4b1

--- GameServerQuery/Config.php
+++ GameServerQuery/Config.php
@@ -24,24 +24,24 @@
  *
  * @category        Net
  * @package         Net_GameServerQuery
- * @author			Aidan Lister <aidan@php.net>
- * @version			$Revision$
+ * @author          Aidan Lister <aidan@php.net>
+ * @version         $Revision$
  */
 class Net_GameServerQuery_Config
 {
-	/**
-	 * An array of all protocol information
-	 *
-	 * @var			array
-	 */
-	private static $_protocol;
+    /**
+     * An array of all protocol information
+     *
+     * @var         array
+     */
+    private static $_protocol;
 
-	/**
-	 * An array of all game information
-	 *
-	 * @var			array
-	 */
-	private static $_game;
+    /**
+     * An array of all game information
+     *
+     * @var         array
+     */
+    private static $_game;
 
 
     /**
@@ -51,35 +51,35 @@ class Net_GameServerQuery_Config
      */
     public function __construct()
     {
-		// Load the protocol config
-		require 'Protocols.php';
-		$this->_protocol = $protocol;
+        // Load the protocol config
+        require 'Protocols.php';
+        $this->_protocol = $protocol;
 
-		// Load the game config
-		require 'Games.php';
-		$this->_game = $game;
+        // Load the game config
+        require 'Games.php';
+        $this->_game = $game;
     }
 
 
-	/**
-	 * Get the packet to be sent to server
-	 *
-	 * This should probably be moved somewhere else, I don't know where.
-	 *
-	 * @return	string	The packet
-	 * @param	string	$game	The game
-	 * @param	string	$query	The type of query
-	 */
-	public function packet($game, $query)
-	{
-		$protocol = $this->_game[$game];
-		$packetformat = $this->_protocol[$protocol]['packet'];
-		$querystring = $this->_protocol[$protocol]['send'][$query];
+    /**
+     * Get the packet to be sent to server
+     *
+     * This should probably be moved somewhere else, I don't know where.
+     *
+     * @return    string    The packet
+     * @param     string    $game     The game
+     * @param     string    $query    The type of query
+     */
+    public function packet($game, $query)
+    {
+        $protocol = $this->_game[$game];
+        $packetformat = $this->_protocol[$protocol]['packet'];
+        $querystring = $this->_protocol[$protocol]['send'][$query];
 
-		$packet = sprintf($packetformat, $querystring);
+        $packet = sprintf($packetformat, $querystring);
 
-		return $packet;
-	}
+        return $packet;
+    }
 
 
     /**
