e1c0a1bade5b9576d2b8e0ed59a53d085159c69b
Aidan Lister <aidan@php.net>
2004-08-03 10:19:32 +0000

    Added Doom3 example
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_GameServerQuery/trunk@165049 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/example1.php
+++ docs/examples/example1.php
@@ -9,9 +9,9 @@ $bm = new Benchmark;
 $bm->start();
 
 // add servers
-$serv1 = $gsq->addServer('bfvietnam', '203.26.94.170', null, 'rules|status|players|ping');
-$serv2 = $gsq->addServer('halflife', '202.173.159.8', null, 'rules|status|players|ping');
-$serv3 = $gsq->addServer('halflife', '202.173.159.8', null, 'rules|status|players|ping');
+//$serv1 = $gsq->addServer('bfvietnam', '203.26.94.170', null, 'rules|status|players|ping');
+//$serv2 = $gsq->addServer('halflife', '202.173.159.8', null, 'rules|status|players|ping');
+$serv3 = $gsq->addServer('doom3', '216.185.106.252', null, 'rules|status|players|ping');
 
 // fire up
 $result = $gsq->execute(300);
@@ -19,14 +19,7 @@ $result = $gsq->execute(300);
 // results
 $bm->stop();
 
-print "<h1>bfvietnam</h1>";
-print_r($result[$serv1]);
-
-print "<h1>halflife</h1>";
-print_r($result[$serv2]);
-
-print "<h1>halflife</h1>";
-print_r($result[$serv3]);
+print_r($result);
 
 // benchmark
 echo 'Took ... ' . $bm->timems() . 'ms';
