0adfbb644ec48190f76cec2ca8c0b9550b75d9ac
Tom Buskens <ortega@php.net>
2004-09-14 11:20:49 +0000

    updated
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_GameServerQuery/trunk@168568 c90b9560-bf6c-de11-be94-00142212c4b1

--- GameServerQuery/Protocol/QuakeWorld.php
+++ GameServerQuery/Protocol/QuakeWorld.php
@@ -32,32 +32,34 @@ class Net_GameServerQuery_Protocol_QuakeWorld extends Net_GameServerQuery_Protoc
 
     protected function _status ()
     {
-                hexdump($this->_response);
-
         // Header
         if (!$this->_match("\xFF\xFF\xFF\xFFn")) {
             throw new Exception('Parsing error');
         }
 
+        // Normal variables
         while ($this->_match('\\([^\\]+)\\([^\\]+)')) {
             $this->_add($this->_result[1], $this->_result[2]);
+        }
+
+        // Seperator
+        if (!$this->_match("\n")) {
+            throw new Exceptions('Parsing error');
+        }
+
+        // Define player variables
+        $vars = array('id', 'score', 'time', 'ping', 'name', 'skin', 'color_top', 'color_bottom');
+        
+        // Parse players
+        while ($this->_match('(-?\d+) (-?\d+) (-?\d+) (-?\d+) \"(.*)\" \"(.*)\" (-?\d+) (-?\d+)')) {
+
+            for ($i = 0; $i !== 8; $i++) {
+                $this->_addPlayer($vars[$i], $this->_result[$i-1]);
+            }
         }
 
         return $this->_output;
 
     }
-
-    protected function _players ()
-    {
-
-
-    }
-
-    protected function _rules ()
-    {
-
-
-    }
 }
-
 ?>
