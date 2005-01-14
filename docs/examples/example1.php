9dec571c7dc1cbb3139ae1bb0ddbf5c39a46b866
Aidan Lister <aidan@php.net>
2005-01-14 14:41:12 +0000

    Formal examples
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_GameServerQuery/trunk@177216 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/example1.php
+++ docs/examples/example1.php
@@ -1,6 +1,13 @@
+<html>
+<head>
+    <title>Net_GSQ Example 1: Basics</title>
+</head>
+<body>
+<p>This example shows the basic usage of Net_GameServerQuery.</p>
+<p>We retrieve rules, status and players from two servers.</p>
 <pre>
 <?php
-include 'Net/GameServerQuery.php';
+require 'Net/GameServerQuery.php';
 
 // load class
 $gsq = new Net_GameServerQuery;
@@ -9,10 +16,12 @@ $gsq = new Net_GameServerQuery;
 $gsq->addServer('bfvietnam', '203.26.94.170', null, 'rules|status|players');
 $gsq->addServer('halflife', '202.173.159.8', null, 'rules|status|players');
 
-// fire up
+// execute
 $result = $gsq->execute(100);
 
 // results
 print_r($result);
 ?>
 </pre>
+</body>
+</html>
---
+++ docs/examples/example3.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Net_GSQ Example 3: Accessing communication classes</title>
+</head>
+<body>
+<p>This example shows how the communication class manually with Net_GameServerQuery.</p>
+<p>We ask a Half-Life server for its status.</p>
+<pre>
+<?php
+require 'Net/GameServerQuery.php';
+
+
+?>
+</pre>
+</body>
+</html>
