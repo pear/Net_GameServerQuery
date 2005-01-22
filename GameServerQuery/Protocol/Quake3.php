87204846d8fbe83b87f8e2e68a8a1ddea837dffc
Aidan Lister <aidan@php.net>
2005-01-22 03:36:07 +0000

    Initial Commit
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_GameServerQuery/trunk@177902 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ GameServerQuery/Protocol/Quake3.php
@@ -0,0 +1,73 @@
+<?php
+// +----------------------------------------------------------------------+
+// | PHP version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2004 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 3.0 of the PHP license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available through the world-wide-web at the following url:           |
+// | http://www.php.net/license/3_0.txt.                                  |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors: Aidan Lister <aidan@php.net>                                |
+// |          Tom Buskens <ortega@php.net>                                |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
+
+require_once NET_GAMESERVERQUERY_BASE . 'Protocol.php';
+
+
+/**
+ * Quake3 Protocol
+ *
+ * @category       Net
+ * @package        Net_GameServerQuery
+ * @author         Aidan Lister <aidan@php.net>
+ * @author         Tom Buskens <ortega@php.net>
+ * @version        $Revision$
+ */
+class Net_GameServerQuery_Protocol_Quake3 extends Net_GameServerQuery_Protocol
+{
+    /**
+     * Rules packet
+     * Players packet
+     */
+    protected function xx(&$buffer, &$result)
+    {
+        return $buffer->getData();
+    }
+    
+    
+    /**
+     * Status packet
+     */
+    protected function info(&$buffer, &$result)
+    {
+        if ($buffer->readInt32() !== -1) {
+            return false;
+        }
+
+        if ($buffer->read(12) !== 'infoResponse') {
+            return false;
+        }
+
+        $result->addMeta('count', $buffer->readInt8());
+        
+        if ($buffer->read() !== '\\') {
+            return false;
+        }
+
+        while (!$buffer->is_empty()) {
+            $result->add($buffer->readString('\\'), $buffer->readString('\\'));
+        }
+
+        return $result->fetch();
+    }
+}
+
+?>
